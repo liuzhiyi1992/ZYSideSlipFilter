ee22f64a0e2576afc62ab643ec9b2e4ceea998a2
liuzhiyi1992 <55137840@qq.com>
2016-10-15 17:07:14 +0800

    虚拟数据源

--- ZYSideSlipFilter/ViewController.m
+++ ZYSideSlipFilter/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "ZYSideSlipFilterController.h"
+#import "ZYSideSlipFilterItemModel.h"
 
 @interface ViewController ()
 @end
@@ -26,7 +27,31 @@
         NSLog(@"");
     }];
     filterController.animationDuration = .3f;
+    filterController.dataList = [self packageDataList];
     [filterController show];
+}
+
+#pragma mark - 模拟数据源
+- (NSArray *)packageDataList {
+    NSMutableArray *dataArray = [NSMutableArray array];
+    [dataArray addObject:[self commonFilterItemModelWithKeywork:@"品牌"]];
+    [dataArray addObject:[self commonFilterItemModelWithKeywork:@"种类"]];
+    [dataArray addObject:[self commonFilterItemModelWithKeywork:@"特性"]];
+    [dataArray addObject:[self commonFilterItemModelWithKeywork:@"适用场景"]];
+    return dataArray;
+}
+
+- (ZYSideSlipFilterItemModel *)commonFilterItemModelWithKeywork:(NSString *)keywork {
+    ZYSideSlipFilterItemModel *model = [[ZYSideSlipFilterItemModel alloc] init];
+    model.containerCellClass = @"SideSlipCommonTableViewCell";
+    model.dataDict = @{@"title":keywork, @"content":@[@{@"itemTitle":@"first", @"itemId":@"0000"},
+                                                         @{@"itemTitle":@"second", @"itemId":@"0001"},
+                                                         @{@"itemTitle":@"third", @"itemId":@"0002"},
+                                                         @{@"itemTitle":@"fourth", @"itemId":@"0003"},
+                                                         @{@"itemTitle":@"fifth", @"itemId":@"0004"},
+                                                         @{@"itemTitle":@"sixth", @"itemId":@"0005"},
+                                                         @{@"itemTitle":@"seventh", @"itemId":@"0006"}]};
+    return model;
 }
 
 - (void)didReceiveMemoryWarning {
