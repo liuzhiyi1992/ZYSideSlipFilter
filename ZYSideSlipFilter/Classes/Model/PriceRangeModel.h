197dfc6389b3306c5fa0737f8f7ec60240d4cafc
liuzhiyi1992 <55137840@qq.com>
2016-10-22 21:53:31 +0800

    价格范围数据保存

--- ZYSideSlipFilter/Classes/Model/PriceRangeModel.h
+++ ZYSideSlipFilter/Classes/Model/PriceRangeModel.h
@@ -8,6 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
+#define PRICE_RANGE_MODEL @"PRICE_RANGE_MODEL"
+
 @interface PriceRangeModel : NSObject
 @property (copy, nonatomic) NSString *minPrice;
 @property (copy, nonatomic) NSString *maxPrice;
--- ZYSideSlipFilter/Classes/View/TableViewCell/SideSlipPriceTableViewCell.m
+++ ZYSideSlipFilter/Classes/View/TableViewCell/SideSlipPriceTableViewCell.m
@@ -7,12 +7,14 @@
 //
 
 #import "SideSlipPriceTableViewCell.h"
+#import "PriceRangeModel.h"
 
 #define TEXTFIELD_MAX_LENGTH 6
 
 @interface SideSlipPriceTableViewCell () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *minTextField;
 @property (weak, nonatomic) IBOutlet UITextField *maxTextField;
+@property (strong, nonatomic) ZYSideSlipFilterRegionModel *itemModel;
 @end
 
 @implementation SideSlipPriceTableViewCell 
@@ -30,7 +32,7 @@
 
 - (void)updateCellWithModel:(ZYSideSlipFilterRegionModel *__autoreleasing *)model
                   indexPath:(NSIndexPath *)indexPath {
-    
+    self.itemModel = *model;
 }
 
 - (void)resetData {
@@ -43,6 +45,14 @@
         return NO;
     }
     return YES;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    PriceRangeModel *model = [[PriceRangeModel alloc] init];
+    model.minPrice = [_minTextField.text stringByReplacingOccurrencesOfString:@" " withString:@""];
+    model.maxPrice = [_maxTextField.text stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSMutableDictionary *mutDict = [NSMutableDictionary dictionaryWithDictionary:_itemModel.customDict];
+    [mutDict setValue:model forKey:PRICE_RANGE_MODEL];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
